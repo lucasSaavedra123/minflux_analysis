8abbbeba6c3d6a69cffd24e504f65e1c2fefdfd9
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2023-12-01 11:14:28 -0300

    Trajectory.py updated

--- Trajectory.py
+++ Trajectory.py
@@ -393,7 +393,7 @@ class Trajectory(Document):
         return self.normalized_ratio <= threshold
 
     def sub_trajectories_trajectories_from_confinement_states(self, v_th=11, window_size=3, transition_fix_threshold=9, use_info=False):
-        confinement_states = self.confinement_states(return_intervals=False, v_th=v_th, window_size=window_size) if not use_info else self.info['analysis']['confinement-states']
+        confinement_states = self.confinement_states(return_intervals=False, v_th=v_th, transition_fix_threshold=transition_fix_threshold, window_size=window_size) if not use_info else self.info['analysis']['confinement-states']
 
         trajectories = {
             0: [],
