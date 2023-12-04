fb735b2d9b7f38564813f5e6f53fd5101aa0b3d7
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2023-12-04 10:35:55 -0300

    03_02 and 03_03 scripts updated

--- scripts/03_03_produce_angles_results.py
+++ scripts/03_03_produce_angles_results.py
@@ -17,8 +17,6 @@ APPLY_GS_CRITERIA = True
 
 DatabaseHandler.connect_over_network(None, None, IP_ADDRESS, COLLECTION_NAME)
 
-basic_info_file = open('./Results/basic_info.txt','w')
-
 new_datasets_list = DATASETS_LIST.copy()[:-3]
 new_datasets_list.append(BTX_NOMENCLATURE)
 new_datasets_list.append(CHOL_NOMENCLATURE)
@@ -139,27 +137,4 @@ for index, dataset in enumerate(new_datasets_list):
             new_angle_information = new_angle_information[cols]
             new_angle_information.to_excel(writer, sheet_name=label, index=False)
 
-basic_info_file.write(f"{BTX_NOMENCLATURE} -> n={Trajectory._get_collection().count_documents({'info.classified_experimental_condition':BTX_NOMENCLATURE})}\n")
-
-fractions = []
-
-for btx_id in Trajectory._get_collection().find({'info.classified_experimental_condition':BTX_NOMENCLATURE}, {f'id':1}):
-    btx_trajectory = Trajectory.objects(id=btx_id['_id'])[0]
-    if 'number_of_confinement_zones' in btx_trajectory.info and btx_trajectory.info['number_of_confinement_zones'] != 0:
-        fractions.append(btx_trajectory.info[f'number_of_confinement_zones_with_{CHOL_NOMENCLATURE}']/btx_trajectory.info['number_of_confinement_zones'])
-
-basic_info_file.write(f"{BTX_NOMENCLATURE} -> Fraction: {np.mean(fractions)}us, S.E.M: {sem(fractions)}s\n")
-
-basic_info_file.write(f"{CHOL_NOMENCLATURE} -> n={Trajectory._get_collection().count_documents({'info.classified_experimental_condition':CHOL_NOMENCLATURE})}\n")
-
-fractions = []
-
-for chol_id in Trajectory._get_collection().find({'info.classified_experimental_condition':CHOL_NOMENCLATURE}, {f'id':1}):
-    chol_trajectory = Trajectory.objects(id=chol_id['_id'])[0]
-    if 'number_of_confinement_zones' in chol_trajectory.info and chol_trajectory.info['number_of_confinement_zones'] != 0:
-        fractions.append(chol_trajectory.info[f'number_of_confinement_zones_with_{BTX_NOMENCLATURE}']/chol_trajectory.info['number_of_confinement_zones'])
-
-basic_info_file.write(f"{CHOL_NOMENCLATURE} -> Fraction: {np.mean(fractions)}us, S.E.M: {sem(fractions)}s\n")
-
-basic_info_file.close()
 DatabaseHandler.disconnect()
