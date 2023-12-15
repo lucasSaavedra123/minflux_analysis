dc764d97d1f936a3eb249bbb5bbe0c8b1b867508
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2023-12-15 00:45:59 -0300

    Code Updated

--- scripts/02_02_measure_intersection.py
+++ scripts/02_02_measure_intersection.py
@@ -11,7 +11,51 @@ from utils import both_trajectories_intersect
 
 DatabaseHandler.connect_over_network(None, None, IP_ADDRESS, COLLECTION_NAME)
 
-files = Trajectory.objects(info__dataset='Cholesterol and btx').distinct(field='info.file')
+#files = Trajectory.objects(info__dataset='Cholesterol and btx').distinct(field='info.file')
+
+files = [
+    '231013-105211_mbm test.txt',
+    '231013-105628_mbm test-pow8pc.txt',
+    '231013-110430_mbm test-pow8pc.txt',
+    '231013-111321_mbm test-pow8pc.txt',
+    '231013-111726_mbm test-pow8pc.txt',
+    '231013-112242_mbm test-pow8pc.txt',
+    '231013-112652_mbm test-pow8pc.txt',
+    '231013-113251_mbm test-pow8pc.txt',
+    '231013-113638_mbm test-pow8pc.txt',
+    '231013-124040_mbm test.txt',
+    '231013-124511_mbm test.txt',
+    '231013-125044_mbm test.txt',
+    '231013-125411_mbm test.txt',
+    '231013-125818_mbm test.txt',
+    '231013-130259_mbm test.txt',
+    '231013-130748_mbm test.txt',
+    '231013-131100_mbm test.txt',
+    '231013-131615_mbm test.txt',
+    '231013-131935_mbm test.txt',
+    '231013-132310_mbm test.txt',
+    '231013-132703_mbm test.txt',
+    '231013-153332_mbm test.txt',
+    '231013-153631_mbm test.txt',
+    '231013-154043_mbm test.txt',
+    '231013-154400_mbm test.txt',
+    '231013-154702_mbm test.txt',
+    '231013-154913_mbm test.txt',
+    '231013-155220_mbm test.txt',
+    '231013-155616_mbm test.txt',
+    '231013-155959_mbm test.txt',
+    '231013-160351_mbm test.txt',
+    '231013-160951_mbm test.txt',
+    '231013-161302_mbm test.txt',
+    '231013-161554_mbm test.txt',
+    '231013-162155_mbm test.txt',
+    '231013-162602_mbm test.txt',
+    '231013-162934_mbm test.txt',
+    '231013-163124_mbm test.txt',
+    '231013-163414_mbm test.txt',
+    '231013-163548_mbm test.txt'
+]
+
 
 for file in tqdm.tqdm(files):
     trajectories = Trajectory.objects(info__file=file)
