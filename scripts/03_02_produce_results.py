9eaef1753037ef692e2835e00cbd920dae6b66c7
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2024-01-19 18:33:44 -0300

    Files Updated

--- scripts/03_02_produce_results.py
+++ scripts/03_02_produce_results.py
@@ -138,23 +138,23 @@ basic_info_file.write(f"{BTX_NOMENCLATURE} -> n={Trajectory._get_collection().co
 
 fractions = []
 
-for btx_id in Trajectory._get_collection().find({'info.classified_experimental_condition':BTX_NOMENCLATURE}, {f'id':1}):
+for btx_id in Trajectory._get_collection().find({'info.classified_experimental_condition':BTX_NOMENCLATURE, 'info.immobile': False}, {f'id':1}):
     btx_trajectory = Trajectory.objects(id=btx_id['_id'])[0]
     if 'number_of_confinement_zones' in btx_trajectory.info and btx_trajectory.info['number_of_confinement_zones'] != 0:
         fractions.append(btx_trajectory.info[f'number_of_confinement_zones_with_{CHOL_NOMENCLATURE}']/btx_trajectory.info['number_of_confinement_zones'])
 
-basic_info_file.write(f"{BTX_NOMENCLATURE} -> Fraction: {np.mean(fractions)}us, S.E.M: {sem(fractions)}s\n")
+basic_info_file.write(f"{BTX_NOMENCLATURE} -> Fraction: {np.mean(fractions)}, S.E.M: {sem(fractions)}s\n")
 
 basic_info_file.write(f"{CHOL_NOMENCLATURE} -> n={Trajectory._get_collection().count_documents({'info.classified_experimental_condition':CHOL_NOMENCLATURE})}\n")
 
 fractions = []
 
-for chol_id in Trajectory._get_collection().find({'info.classified_experimental_condition':CHOL_NOMENCLATURE}, {f'id':1}):
+for chol_id in Trajectory._get_collection().find({'info.classified_experimental_condition':CHOL_NOMENCLATURE, 'info.immobile': False}, {f'id':1}):
     chol_trajectory = Trajectory.objects(id=chol_id['_id'])[0]
     if 'number_of_confinement_zones' in chol_trajectory.info and chol_trajectory.info['number_of_confinement_zones'] != 0:
         fractions.append(chol_trajectory.info[f'number_of_confinement_zones_with_{BTX_NOMENCLATURE}']/chol_trajectory.info['number_of_confinement_zones'])
 
-basic_info_file.write(f"{CHOL_NOMENCLATURE} -> Fraction: {np.mean(fractions)}us, S.E.M: {sem(fractions)}s\n")
+basic_info_file.write(f"{CHOL_NOMENCLATURE} -> Fraction: {np.mean(fractions)}, S.E.M: {sem(fractions)}s\n")
 
 basic_info_file.close()
 DatabaseHandler.disconnect()
