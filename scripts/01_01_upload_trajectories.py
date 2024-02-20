e264b92433ef85ed12d03d19f8eedb4aae565790
lucassaavedra123 <lucasarielsaavedra@hotmail.com>
2024-02-19 20:49:39 -0500

    01_01 script updated

--- scripts/01_01_upload_trajectories.py
+++ scripts/01_01_upload_trajectories.py
@@ -1,7 +1,6 @@
 """
 This script upload all trajectories to the MongoDB database.
 """
-
 import os
 from collections import defaultdict
 
@@ -15,6 +14,19 @@ from CONSTANTS import *
 
 DatabaseHandler.connect_over_network(None, None, IP_ADDRESS, 'MINFLUX_DATA')
 
+trajectories_info = list(Trajectory._get_collection().find({}, {
+    'info.dataset':1,
+    'info.file':1,
+    'info.trajectory_id':1
+}))
+
+def check_query_equal_to_document(document, query):
+    dataset_equal = document['info']['dataset'] == query['info.dataset']
+    file_equal = document['info']['file'] == query['info.file']
+    trajectory_id_equal = document['info']['trajectory_id'] == query['info.trajectory_id']
+
+    return dataset_equal and file_equal and trajectory_id_equal
+
 def trajectory_was_already_uploaded(dataset, file, trajectory_id):
     a_query = {
         'info.dataset': dataset,
@@ -22,7 +34,8 @@ def trajectory_was_already_uploaded(dataset, file, trajectory_id):
         'info.trajectory_id': trajectory_id,
     }
 
-    number_of_trajectories = Trajectory._get_collection().count_documents(a_query)
+    #number_of_trajectories = Trajectory._get_collection().count_documents(a_query)
+    number_of_trajectories = len([d for d in trajectories_info if check_query_equal_to_document(d, a_query)])
 
     assert number_of_trajectories <= 1
     return number_of_trajectories == 1
@@ -43,8 +56,12 @@ def upload_trajectory_from_track_dataset(track_dataset, dataset_directory, file_
         ).save()
 
 def extract_dataframes_from_file(dataset_directory, a_file):
-    print(f"Extracting info from dataset {dataset_directory} and file {a_file}")
-    dataset = pd.read_csv(os.path.join(dataset_directory,a_file), sep=' ', header=None)
+    #print(f"Extracting info from dataset {dataset_directory} and file {a_file}")
+    
+    try:
+        dataset = pd.read_csv(os.path.join(dataset_directory,a_file), sep=' ', header=None)
+    except pd.errors.EmptyDataError:
+        return []
 
     if len(dataset.columns) == 6:
         dataset = dataset.rename(columns={index: value for index, value in enumerate(['track_id', 't', 'x', 'y', 'intensity', 'dcr'])})
@@ -62,7 +79,7 @@ def extract_dataframes_from_file(dataset_directory, a_file):
 
     extraction_result = []
 
-    for row_index in tqdm.tqdm(list(range(len(dataset)))):
+    for row_index in list(range(len(dataset))):
         if dataset.iloc[row_index]['track_id'] != current_id:
             extraction_result.append((
                 dataset.iloc[initial_row:row_index].copy().sort_values('t', ascending=True),
@@ -85,12 +102,12 @@ def extract_dataframes_from_file(dataset_directory, a_file):
     return extraction_result
 
 def upload_trajectories_from_directory(dataset_directory):
-    print('Uploading Trajectories from dataset directory', dataset_directory)
+    #print('Uploading Trajectories from dataset directory', dataset_directory)
 
     for a_file in tqdm.tqdm([file for file in os.listdir(dataset_directory) if file.endswith('.txt')]):
         extraction_result = extract_dataframes_from_file(dataset_directory, a_file)
-        print(f"Uploading trajectories from dataset {dataset_directory} and file {a_file}")
-        for info_extracted in tqdm.tqdm(extraction_result):
+        #print(f"Uploading trajectories from dataset {dataset_directory} and file {a_file}")
+        for info_extracted in extraction_result:
             upload_trajectory_from_track_dataset(
                 info_extracted[0],
                 dataset_directory,
@@ -101,6 +118,7 @@ def upload_trajectories_from_directory(dataset_directory):
     print('All trajectories uploaded from dataset directory', dataset_directory)
 
 for dataset in DATASETS_LIST:
+    print(dataset)
     upload_trajectories_from_directory(dataset)
 
 DatabaseHandler.disconnect()
