5b0e2e1239e726a334ec8c2520400358478c816e
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2024-03-10 18:43:53 -0300

    01_02 updated

--- scripts/01_02_dcr_analysis.py
+++ scripts/01_02_dcr_analysis.py
@@ -17,21 +17,19 @@ def analyze_dcr(datasets_one, datasets_two, datasets_combined, dataset_one_label
     plt.rcParams['savefig.dpi'] = 500
     DatabaseHandler.connect_over_network(None, None, IP_ADDRESS, COLLECTION_NAME)
 
-    btx_dcr_values = []
-    chol_dcr_values = []
+    dataset_one_dcr_values = []
+    dataset_two_dcr_values = []
 
     for dataset_one in datasets_one:
-        btx_dcr_values += [np.mean(dcr_values) for dcr_values in get_list_of_values_of_field({'info.dataset': dataset_one}, 'dcr')]
+        dataset_one_dcr_values += [np.mean(dcr_values) for dcr_values in get_list_of_values_of_field({'info.dataset': dataset_one}, 'dcr')]
 
     for dataset_two in datasets_two:
-        chol_dcr_values += [np.mean(dcr_values) for dcr_values in get_list_of_values_of_field({'info.dataset': dataset_two}, 'dcr')]
+        dataset_two_dcr_values += [np.mean(dcr_values) for dcr_values in get_list_of_values_of_field({'info.dataset': dataset_two}, 'dcr')]
 
     dcr_dataframe = pd.DataFrame({
-        'Track detection channel ratio (tDCR)': btx_dcr_values + chol_dcr_values,
-        'Experimental condition': [dataset_one_label] * len(btx_dcr_values) + [dataset_two_label] * len(chol_dcr_values)
+        'Track detection channel ratio (tDCR)': dataset_one_dcr_values + dataset_two_dcr_values,
+        'Experimental condition': [dataset_one_label] * len(dataset_one_dcr_values) + [dataset_two_label] * len(dataset_two_dcr_values)
     })
-
-    DatabaseHandler.disconnect()
 
     sns.set(font_scale=3.5)
     sns.histplot(data=dcr_dataframe, x='Track detection channel ratio (tDCR)', hue='Experimental condition', kde=True)
@@ -39,21 +37,21 @@ def analyze_dcr(datasets_one, datasets_two, datasets_combined, dataset_one_label
 
     DatabaseHandler.connect_over_network(None, None, IP_ADDRESS, COLLECTION_NAME)
 
-    chol_and_btx_dcr_values = []
+    datasets_combined_dcr_values = []
 
     for dataset_combined in datasets_combined:
-        chol_and_btx_dcr_values += [np.mean(dcr_values) for dcr_values in get_list_of_values_of_field({'info.dataset': dataset_combined}, 'dcr')]
+        datasets_combined_dcr_values += [np.mean(dcr_values) for dcr_values in get_list_of_values_of_field({'info.dataset': dataset_combined}, 'dcr')]
 
     dcr_dataframe = pd.DataFrame({
-        'Track detection channel ratio (tDCR)': chol_and_btx_dcr_values,
-        'Experimental condition': [f'{dataset_two_label}(+{dataset_one_label})' + "\n+\n" + f'{dataset_one_label}(+{dataset_two_label})'] * len(chol_and_btx_dcr_values)
+        'Track detection channel ratio (tDCR)': datasets_combined_dcr_values,
+        'Experimental condition': [f'{dataset_two_label}(+{dataset_one_label})' + "\n+\n" + f'{dataset_one_label}(+{dataset_two_label})'] * len(datasets_combined_dcr_values)
     })
-
-    DatabaseHandler.disconnect()
 
     sns.set(font_scale=3.5)
     sns.histplot(data=dcr_dataframe, x='Track detection channel ratio (tDCR)', color='#805380', edgecolor='#634163', kde=True)
     plt.show()
+
+    DatabaseHandler.disconnect()
 
 btx_alone_datasets = ['BTX680R', 'CK666-BTX680']
 chol_alone_datasets = ['CholesterolPEGKK114', 'CK666-CHOL']
