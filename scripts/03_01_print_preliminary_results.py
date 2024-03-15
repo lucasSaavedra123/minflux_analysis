5e3260cc30cf53de2b3ded6f387f43d23c48fc90
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2024-03-15 12:13:18 -0300

    03_01 code updated

--- scripts/03_01_print_preliminary_results.py
+++ scripts/03_01_print_preliminary_results.py
@@ -17,8 +17,6 @@ from CONSTANTS import *
 from utils import *
 
 
-APPLY_GS_CRITERIA = True
-
 INDIVIDUAL_DATASETS = [
     'Control',
     'CDx',
@@ -39,44 +37,30 @@ COMBINED_DATASETS = [
 
 DatabaseHandler.connect_over_network(None, None, IP_ADDRESS, COLLECTION_NAME)
 file = open('./preliminary_results.txt', 'w')
-"""
+
 file.write("PERCENTAGE OF IMMOBILIZED TRAJECTORIES\n")
-quantity = Trajectory._get_collection().count_documents({'info.dataset':DATASETS_LIST[0]})
-immobile_quantity = Trajectory._get_collection().count_documents({'info.dataset':DATASETS_LIST[0], 'info.immobile':True})
-file.write(DATASETS_LIST[0], immobile_quantity/quantity, "\n")
+for dataset in tqdm.tqdm(INDIVIDUAL_DATASETS):
+    quantity = Trajectory._get_collection().count_documents({'info.dataset':dataset})
+    immobile_quantity = Trajectory._get_collection().count_documents({'info.dataset':dataset, 'info.immobile':True})
+    file.write(f'{dataset} {immobile_quantity/quantity}\n')
 
-quantity = Trajectory._get_collection().count_documents({'info.dataset':DATASETS_LIST[1]})
-immobile_quantity = Trajectory._get_collection().count_documents({'info.dataset':DATASETS_LIST[1], 'info.immobile':True})
-file.write(DATASETS_LIST[1], immobile_quantity/quantity, "\n")
+for dataset in tqdm.tqdm(COMBINED_DATASETS):
+    quantity = Trajectory._get_collection().count_documents({'info.dataset': dataset, 'info.classified_experimental_condition': BTX_NOMENCLATURE})
+    immobile_quantity = Trajectory._get_collection().count_documents({'info.dataset':dataset, 'info.classified_experimental_condition': BTX_NOMENCLATURE, 'info.immobile':True})
+    file.write(f'({BTX_NOMENCLATURE}) {dataset} {immobile_quantity/quantity}\n')
 
-quantity = Trajectory._get_collection().count_documents({'info.dataset':DATASETS_LIST[2]})
-immobile_quantity = Trajectory._get_collection().count_documents({'info.dataset':DATASETS_LIST[2], 'info.immobile':True})
-file.write(DATASETS_LIST[2], immobile_quantity/quantity, "\n")
-
-quantity = Trajectory._get_collection().count_documents({'info.dataset':DATASETS_LIST[3]})
-immobile_quantity = Trajectory._get_collection().count_documents({'info.dataset':DATASETS_LIST[3], 'info.immobile':True})
-file.write(DATASETS_LIST[3], immobile_quantity/quantity, "\n")
-
-quantity = Trajectory._get_collection().count_documents({'info.classified_experimental_condition':BTX_NOMENCLATURE})
-immobile_quantity = Trajectory._get_collection().count_documents({'info.classified_experimental_condition':BTX_NOMENCLATURE, 'info.immobile':True})
-file.write(BTX_NOMENCLATURE, immobile_quantity/quantity, "\n")
-
-quantity = Trajectory._get_collection().count_documents({'info.classified_experimental_condition':CHOL_NOMENCLATURE})
-immobile_quantity = Trajectory._get_collection().count_documents({'info.classified_experimental_condition':CHOL_NOMENCLATURE, 'info.immobile':True})
-file.write(CHOL_NOMENCLATURE, immobile_quantity/quantity, "\n")
+    quantity = Trajectory._get_collection().count_documents({'info.dataset': dataset, 'info.classified_experimental_condition': CHOL_NOMENCLATURE})
+    immobile_quantity = Trajectory._get_collection().count_documents({'info.dataset':dataset, 'info.classified_experimental_condition': CHOL_NOMENCLATURE, 'info.immobile':True})
+    file.write(f'({CHOL_NOMENCLATURE}) {dataset} {immobile_quantity/quantity}\n')
 
 file.write("RATIOS\n")
-pd.DataFrame({'ratio': get_list_of_values_of_field({'info.dataset': 'Control'}, 'ratio')}).to_csv('Results/control_ratios.csv')
-pd.DataFrame({'ratio': get_list_of_values_of_field({'info.dataset': 'CDx'}, 'ratio')}).to_csv('Results/cdx_ratios.csv')
+for dataset in tqdm.tqdm(INDIVIDUAL_DATASETS):
+    pd.DataFrame({'ratio': get_list_of_values_of_field({'info.dataset': dataset}, 'ratio')}).to_csv(f'Results/{dataset}_ratios.csv')
 
-ratios = get_list_of_values_of_field({'info.dataset': 'BTX680R'}, 'ratio')
-ratios += get_list_of_values_of_field({'info.classified_experimental_condition': BTX_NOMENCLATURE}, 'ratio')
-pd.DataFrame({'ratio': ratios}).to_csv('Results/btx_ratios.csv')
+for dataset in tqdm.tqdm(COMBINED_DATASETS):
+    pd.DataFrame({'ratio': get_list_of_values_of_field({'info.dataset': dataset, 'info.classified_experimental_condition': BTX_NOMENCLATURE}, 'ratio')}).to_csv(f'Results/{dataset}_{BTX_NOMENCLATURE}_ratios.csv')
+    pd.DataFrame({'ratio': get_list_of_values_of_field({'info.dataset': dataset, 'info.classified_experimental_condition': CHOL_NOMENCLATURE}, 'ratio')}).to_csv(f'Results/{dataset}_{CHOL_NOMENCLATURE}_ratios.csv')
 
-ratios = get_list_of_values_of_field({'info.dataset': 'CholesterolPEGKK114'}, 'ratio')
-ratios += get_list_of_values_of_field({'info.classified_experimental_condition': CHOL_NOMENCLATURE}, 'ratio')
-pd.DataFrame({'ratio': ratios}).to_csv('Results/chol_ratios.csv')
-"""
 file.write("ALL INTERVALS\n")
 for dataset in tqdm.tqdm(INDIVIDUAL_DATASETS):
     list_of_trajectories_time = get_list_of_main_field({'info.dataset': dataset}, 't')
@@ -157,7 +141,6 @@ for dataset in tqdm.tqdm(COMBINED_DATASETS):
     durations = [time_list[-1] - time_list[0] for time_list in list_of_trajectories_time]
     durations = [duration for duration in durations if duration != 0]
     file.write(f'{CHOL_NOMENCLATURE} ({dataset}) (n={len(durations)})-> Intervals {np.mean(durations)} {sem(durations)}\n')
-
 
 file.close()
 DatabaseHandler.disconnect()
