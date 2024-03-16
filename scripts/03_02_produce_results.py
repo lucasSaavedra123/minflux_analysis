b06ecc75d56334475001eecd5b0e419b40f52564
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2024-03-16 12:47:49 -0300

    03_02 updated

--- scripts/03_02_produce_results.py
+++ scripts/03_02_produce_results.py
@@ -45,8 +45,7 @@ for combined_dataset in [
 
 for index, dataset in enumerate(new_datasets_list):
     print(dataset,index)
-    if index < len(INDIVIDUAL_DATASETS):
-        continue
+
     basic_query_dict = {'info.dataset': dataset} if index < len(INDIVIDUAL_DATASETS) else {'info.dataset': dataset[0], 'info.classified_experimental_condition':dataset[1]}
     with pd.ExcelWriter(f"./Results/{dataset}_{index}_gs_{APPLY_GS_CRITERIA}_basic_information.xlsx") as writer:
         #Data that take into account GS criteria
@@ -55,9 +54,9 @@ for index, dataset in enumerate(new_datasets_list):
 
         pd.DataFrame({'k': get_list_of_values_of_analysis_field(filter_query, 'k')}).to_excel(writer, sheet_name='k', index=False)
         pd.DataFrame({'betha': get_list_of_values_of_analysis_field(filter_query, 'betha')}).to_excel(writer, sheet_name='betha', index=False)
-        pd.DataFrame({'dc': get_list_of_values_of_analysis_field(filter_query, 'directional_coefficient')}).to_excel(writer, sheet_name='directional_coefficient', index=False)
-        pd.DataFrame({'d_2_4': get_list_of_values_of_analysis_field(filter_query, 'd_2_4')}).to_excel(writer, sheet_name='d_2_4', index=False)
-        pd.DataFrame({'localization_precision': get_list_of_values_of_analysis_field(filter_query, 'localization_precision')}).to_excel(writer, sheet_name='localization_precision', index=False)
+        #pd.DataFrame({'dc': get_list_of_values_of_analysis_field(filter_query, 'directional_coefficient')}).to_excel(writer, sheet_name='directional_coefficient', index=False)
+        #pd.DataFrame({'d_2_4': get_list_of_values_of_analysis_field(filter_query, 'd_2_4')}).to_excel(writer, sheet_name='d_2_4', index=False)
+        #pd.DataFrame({'localization_precision': get_list_of_values_of_analysis_field(filter_query, 'localization_precision')}).to_excel(writer, sheet_name='localization_precision', index=False)
 
         residence_times = get_list_of_values_of_analysis_field(filter_query, 'residence_time')
         residence_times = [residence_time for residence_time in residence_times if residence_time != 0]
@@ -81,7 +80,7 @@ for index, dataset in enumerate(new_datasets_list):
         list_of_semi_major_axis = get_list_of_values_of_analysis_field(filter_query, 'confinement-a')
         list_of_semi_major_axis = list(itertools.chain.from_iterable(list_of_semi_major_axis))
         pd.DataFrame({'semi_major_axis': list_of_semi_major_axis}).to_excel(writer, sheet_name='semi_major_axis', index=False)
-        """
+
         browian_portions = []
         confined_portions = []
 
@@ -111,7 +110,7 @@ for index, dataset in enumerate(new_datasets_list):
         durations = [time_list[-1] - time_list[0] for time_list in list_of_trajectories_time]
         durations = [duration for duration in durations if duration != 0]
         pd.DataFrame({'duration': durations}).to_excel(writer, sheet_name='duration', index=False)
-
+        """
         #Data that takes all mobile trajectories
         filter_query = basic_query_dict.copy()
         filter_query.update({'info.immobile': False})
@@ -155,30 +154,31 @@ for index, dataset in enumerate(new_datasets_list):
         list_of_confinement_areas_centroids = get_list_of_values_of_analysis_field(filter_query, 'confinement_areas_centroids')
         list_of_confinement_areas_centroids = list(itertools.chain.from_iterable([pdist(np.array(confinement_areas_centroids) * 1e3).tolist() for confinement_areas_centroids in list_of_confinement_areas_centroids if len(confinement_areas_centroids) >= 2]))
         pd.DataFrame({'confinement_areas_distance': list_of_confinement_areas_centroids}).to_csv(f'./Results/{dataset}_{index}_confinement_areas_distance.csv')
-"""
+
 basic_info_file = open('./Results/basic_info.txt','w')
-basic_info_file.write(f"{BTX_NOMENCLATURE} -> n={Trajectory._get_collection().count_documents({'info.classified_experimental_condition':BTX_NOMENCLATURE})}\n")
 
-fractions = []
+for combined_dataset in [
+    'Cholesterol and btx',
+    'CK666-BTX680-CHOL',
+    'BTX680-fPEG-CHOL-50-nM',
+    'BTX680-fPEG-CHOL-100-nM',
+]:
+    fractions = []
+    for btx_id in Trajectory._get_collection().find({'info.dataset': combined_dataset, 'info.classified_experimental_condition':BTX_NOMENCLATURE, 'info.immobile': False}, {f'id':1}):
+        btx_trajectory = Trajectory.objects(id=btx_id['_id'])[0]
+        if 'number_of_confinement_zones' in btx_trajectory.info and btx_trajectory.info['number_of_confinement_zones'] != 0:
+            fractions.append(btx_trajectory.info[f'number_of_confinement_zones_with_{CHOL_NOMENCLATURE}']/btx_trajectory.info['number_of_confinement_zones'])
 
-for btx_id in Trajectory._get_collection().find({'info.classified_experimental_condition':BTX_NOMENCLATURE, 'info.immobile': False}, {f'id':1}):
-    btx_trajectory = Trajectory.objects(id=btx_id['_id'])[0]
-    if 'number_of_confinement_zones' in btx_trajectory.info and btx_trajectory.info['number_of_confinement_zones'] != 0:
-        fractions.append(btx_trajectory.info[f'number_of_confinement_zones_with_{CHOL_NOMENCLATURE}']/btx_trajectory.info['number_of_confinement_zones'])
+    basic_info_file.write(f"{combined_dataset}-{BTX_NOMENCLATURE} Fraction: {np.mean(fractions)}, S.E.M: {sem(fractions)}s\n")
 
-basic_info_file.write(f"{BTX_NOMENCLATURE} -> Fraction: {np.mean(fractions)}, S.E.M: {sem(fractions)}s\n")
+    fractions = []
+    for chol_id in Trajectory._get_collection().find({'info.dataset': combined_dataset, 'info.classified_experimental_condition':CHOL_NOMENCLATURE, 'info.immobile': False}, {f'id':1}):
+        chol_trajectory = Trajectory.objects(id=chol_id['_id'])[0]
+        if 'number_of_confinement_zones' in chol_trajectory.info and chol_trajectory.info['number_of_confinement_zones'] != 0:
+            fractions.append(chol_trajectory.info[f'number_of_confinement_zones_with_{BTX_NOMENCLATURE}']/chol_trajectory.info['number_of_confinement_zones'])
 
-basic_info_file.write(f"{CHOL_NOMENCLATURE} -> n={Trajectory._get_collection().count_documents({'info.classified_experimental_condition':CHOL_NOMENCLATURE})}\n")
-
-fractions = []
-
-for chol_id in Trajectory._get_collection().find({'info.classified_experimental_condition':CHOL_NOMENCLATURE, 'info.immobile': False}, {f'id':1}):
-    chol_trajectory = Trajectory.objects(id=chol_id['_id'])[0]
-    if 'number_of_confinement_zones' in chol_trajectory.info and chol_trajectory.info['number_of_confinement_zones'] != 0:
-        fractions.append(chol_trajectory.info[f'number_of_confinement_zones_with_{BTX_NOMENCLATURE}']/chol_trajectory.info['number_of_confinement_zones'])
-
-basic_info_file.write(f"{CHOL_NOMENCLATURE} -> Fraction: {np.mean(fractions)}, S.E.M: {sem(fractions)}s\n")
+    basic_info_file.write(f"{combined_dataset}-{CHOL_NOMENCLATURE} Fraction: {np.mean(fractions)}, S.E.M: {sem(fractions)}s\n")
 
 basic_info_file.close()
-"""
+
 DatabaseHandler.disconnect()
