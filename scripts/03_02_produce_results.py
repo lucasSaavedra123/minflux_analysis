135b1cedac359b9494bfd757844e5ea8ee11e2b9
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2024-04-07 15:57:43 -0300

    Code Updated git push

--- scripts/03_02_produce_results.py
+++ scripts/03_02_produce_results.py
@@ -202,4 +202,36 @@ for combined_dataset in [
 
 basic_info_file.close()
 
+overlap_non_confinement_portion_info = open('./Results/overlap_non_confinement_portion_info.txt','w')
+
+for combined_dataset in [
+    'Cholesterol and btx',
+    'CK666-BTX680-CHOL',
+    'BTX680-fPEG-CHOL-50-nM',
+    'BTX680-fPEG-CHOL-100-nM',
+]:
+    fractions = []
+    for btx_id in Trajectory._get_collection().find({'info.dataset': combined_dataset, 'info.classified_experimental_condition':BTX_NOMENCLATURE, 'info.immobile': False}, {f'id':1}):
+        btx_trajectory = Trajectory.objects(id=btx_id['_id'])[0]
+        
+        if f'{CHOL_NOMENCLATURE}_single_intersections' in btx_trajectory.info:
+            for non_confined_portion in btx_trajectory.sub_trajectories_trajectories_from_confinement_states(v_th=33, use_info=True)[0]:
+                fractions.append(np.sum(non_confined_portion.info[f'{CHOL_NOMENCLATURE}_single_intersections'])/non_confined_portion.length)
+
+    np.savetxt(f'./Results/{combined_dataset}-{BTX_NOMENCLATURE}_non_confinement_overlap_fraction.txt', fractions, fmt='%f')
+    overlap_non_confinement_portion_info.write(f"{combined_dataset}-{BTX_NOMENCLATURE} Fraction: {np.mean(fractions)}, S.E.M: {sem(fractions)}s\n")
+
+    fractions = []
+    for chol_id in Trajectory._get_collection().find({'info.dataset': combined_dataset, 'info.classified_experimental_condition':CHOL_NOMENCLATURE, 'info.immobile': False}, {f'id':1}):
+        chol_trajectory = Trajectory.objects(id=chol_id['_id'])[0]
+
+        if f'{BTX_NOMENCLATURE}_single_intersections' in btx_trajectory.info:
+            for non_confined_portion in btx_trajectory.sub_trajectories_trajectories_from_confinement_states(v_th=33, use_info=True)[0]:
+                fractions.append(np.sum(non_confined_portion.info[f'{BTX_NOMENCLATURE}_single_intersections'])/non_confined_portion.length)
+    
+    np.savetxt(f'./Results/{combined_dataset}-{CHOL_NOMENCLATURE}_non_confinement_overlap_fraction.txt', fractions, fmt='%f')
+    overlap_non_confinement_portion_info.write(f"{combined_dataset}-{CHOL_NOMENCLATURE} Fraction: {np.mean(fractions)}, S.E.M: {sem(fractions)}s\n")
+
+overlap_non_confinement_portion_info.close()
+
 DatabaseHandler.disconnect()
