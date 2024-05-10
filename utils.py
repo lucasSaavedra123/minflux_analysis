f3f549fcf2f3f3f88e1054d8e91ed84466203554
lucassaavedra123 <lucasarielsaavedra@hotmail.com>
2024-05-10 11:45:47 -0300

    Code Updated

--- utils.py
+++ utils.py
@@ -434,11 +434,11 @@ def equation_confined(x, DU, L_HOP, LOCALIZATION_PRECISION):
     return equation_hop(x, 0, DU, L_HOP, LOCALIZATION_PRECISION)
 
 def free_fitting(X,Y):
-    select_indexes = np.unique(np.geomspace(1,len(X),len(X)//2).astype(int))-1
+    select_indexes = np.unique(np.geomspace(1,len(X),len(X)//4).astype(int))-1
     X = X[select_indexes]
     Y = Y[select_indexes]
 
-    def eq_4_obj_raw(x, y, d, delta): return np.sum((1/x)*(y - equation_free(x, d, delta))**2)
+    def eq_4_obj_raw(x, y, d, delta): return np.sum((y - equation_free(x, d, delta))**2)
     #def eq_4_obj_raw(x, y, d, delta): return np.sum((y - equation_free(x, d, delta))**2)
 
     eq_4_obj = lambda coeffs: eq_4_obj_raw(X, Y, *coeffs)
@@ -452,11 +452,11 @@ def free_fitting(X,Y):
     return min(res_eq_4s, key=lambda r: r.fun)
 
 def hop_fitting(X,Y):
-    select_indexes = np.unique(np.geomspace(1,len(X),len(X)//2).astype(int))-1
+    select_indexes = np.unique(np.geomspace(1,len(X),len(X)//4).astype(int))-1
     X = X[select_indexes]
     Y = Y[select_indexes]
 
-    def eq_9_obj_raw(x, y, dm, du, l_hop, delta): return np.sum((1/x)*(y - equation_hop(x, dm, du, l_hop, delta))**2)
+    def eq_9_obj_raw(x, y, dm, du, l_hop, delta): return np.sum((y - equation_hop(x, dm, du, l_hop, delta))**2)
     eq_9_obj = lambda coeffs: eq_9_obj_raw(X, Y, *coeffs)
     res_eq_9s = []
 
@@ -468,11 +468,11 @@ def hop_fitting(X,Y):
     return min(res_eq_9s, key=lambda r: r.fun)
 
 def confined_fitting(X,Y):
-    select_indexes = np.unique(np.geomspace(1,len(X),len(X)//2).astype(int))-1
+    select_indexes = np.unique(np.geomspace(1,len(X),len(X)//4).astype(int))-1
     X = X[select_indexes]
     Y = Y[select_indexes]
 
-    def eq_9_obj_raw(x, y, du, l, delta): return np.sum((1/x)*(y - equation_confined(x, du, l, delta))**2)
+    def eq_9_obj_raw(x, y, du, l, delta): return np.sum((y - equation_confined(x, du, l, delta))**2)
     eq_9_obj = lambda coeffs: eq_9_obj_raw(X, Y, *coeffs)
     res_eq_9s = []
 
