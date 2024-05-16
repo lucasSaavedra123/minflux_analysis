5ba7734f3517265b8d0c3c718ffbd729d53f1577
lucassaavedra123 <lucasarielsaavedra@hotmail.com>
2024-05-16 19:09:27 -0300

    Code Updated

--- scripts/02_03_overlap_quantification.py
+++ scripts/02_03_overlap_quantification.py
@@ -6,7 +6,6 @@ of Chol confinement.
 import itertools
 
 import tqdm
-import ray
 import numpy as np
 
 from DatabaseHandler import DatabaseHandler
@@ -22,35 +21,6 @@ CHOL_AND_BTX_DATASETS = [
     'BTX680-fPEG-CHOL-100-nM',
 ]
 
-
-@ray.remote
-def analyze_trajectory(trajectory_id):
-    DatabaseHandler.connect_over_network(None, None, IP_ADDRESS, COLLECTION_NAME)
-
-    trajectory = Trajectory.objects(id=trajectory_id['_id'])[0]
-
-    if 'analysis' not in trajectory.info:
-        return None
-
-    confinements = trajectory.sub_trajectories_trajectories_from_confinement_states(v_th=33, use_info=False)[1]
-
-    other_trajectories = Trajectory.objects(id__in=trajectory.info[f'intersections_with_others'])
-    other_confinements = [other_trajectory.sub_trajectories_trajectories_from_confinement_states(v_th=33, use_info=False)[1] for other_trajectory in other_trajectories]
-    other_confinements = list(itertools.chain.from_iterable(other_confinements))
-
-    trajectory.info['number_of_confinement_zones'] = len(confinements)
-    trajectory.info[f'number_of_confinement_zones_with_others'] = 0
-
-    for btx_confinement in confinements:
-        there_is_overlap = any([both_trajectories_intersect(chol_confinement, btx_confinement, via='hull') for chol_confinement in other_confinements])
-        trajectory.info[f'number_of_confinement_zones_with_others'] += 1 if there_is_overlap else 0
-    
-    assert trajectory.info[f'number_of_confinement_zones_with_others'] <= trajectory.info['number_of_confinement_zones']
-    trajectory.save()
-
-    DatabaseHandler.disconnect()
-
-@ray.remote
 def analyze_dataset_and_roi(dataset, file, roi):
     DatabaseHandler.connect_over_network(None, None, IP_ADDRESS, COLLECTION_NAME)
     trajectories = Trajectory.objects(info__file=file, info__dataset=dataset, info__roi=roi)
@@ -74,10 +44,8 @@ def analyze_dataset_and_roi(dataset, file, roi):
 
         for chol_confinement in chol_confinements:
             for btx_confinement in btx_confinements:
-                print(np.linalg.norm(chol_confinement.centroid-btx_confinement.centroid))
-                if np.linalg.norm(chol_confinement.centroid-btx_confinement.centroid) < 1:#um
-                    there_is_overlap = both_trajectories_intersect(chol_confinement, btx_confinement, via='hull')
-                    chol_trajectory.info[f'number_of_confinement_zones_with_{BTX_NOMENCLATURE}'] += 1 if there_is_overlap else 0
+                if np.linalg.norm(chol_confinement.centroid-btx_confinement.centroid) < 1 and both_trajectories_intersect(chol_confinement, btx_confinement, via='hull'):#um
+                    chol_trajectory.info[f'number_of_confinement_zones_with_{BTX_NOMENCLATURE}'] += 1
                     break
         assert chol_trajectory.info[f'number_of_confinement_zones_with_{BTX_NOMENCLATURE}'] <= chol_trajectory.info['number_of_confinement_zones']
         chol_trajectory.save()
@@ -92,18 +60,19 @@ def analyze_dataset_and_roi(dataset, file, roi):
 
         for btx_confinement in btx_confinements:
             for chol_confinement in chol_confinements:
-                if np.linalg.norm(chol_confinement.centroid-btx_confinement.centroid) < 1:#um
-                    there_is_overlap = both_trajectories_intersect(chol_confinement, btx_confinement, via='hull')
-                    btx_trajectory.info[f'number_of_confinement_zones_with_{CHOL_NOMENCLATURE}'] += 1 if there_is_overlap else 0
+                if np.linalg.norm(chol_confinement.centroid-btx_confinement.centroid) < 1 and both_trajectories_intersect(chol_confinement, btx_confinement, via='hull'):#um
+                    btx_trajectory.info[f'number_of_confinement_zones_with_{CHOL_NOMENCLATURE}'] += 1
                     break
         assert btx_trajectory.info[f'number_of_confinement_zones_with_{CHOL_NOMENCLATURE}'] <= btx_trajectory.info['number_of_confinement_zones']
         btx_trajectory.save()
 
     DatabaseHandler.disconnect()
 
+
 file_and_rois = [info for info in extract_dataset_file_roi_file() if info[0] in CHOL_AND_BTX_DATASETS]
 
-ray.get([analyze_dataset_and_roi.remote(dataset, file, roi) for dataset,file,roi in file_and_rois])
+for file_and_roi in tqdm.tqdm(file_and_rois):
+    analyze_dataset_and_roi(file_and_roi[0], file_and_roi[1], file_and_roi[2])
 
 """
 analyzed_ids += chol_trajectories_ids
